05f5b153e5535a4ec438dfc2fdd9ecbb6001f4e6
smokeeaasd <lucas.gomes262@etec.sp.gov.br>
2023-07-31 14:39:55 -0300

    corrigindo movimento da camera

--- Assets/Script/CameraFollow.cs
+++ Assets/Script/CameraFollow.cs
@@ -7,31 +7,31 @@ public class CameraFollow : MonoBehaviour
 {
     public Transform target1;
     public Transform target2;
-    public float smoothSpeed = 0.125f;
     public Vector3 offset;
+
+    public float smoothSpeed = 0.125f;
     public float yOffset = 0f;
 
     public Transform wallLeft;
     public Transform wallRight;
-	
+
     private Vector3 desiredPosition;
+    private Vector3 initialCameraPosition;
 
     private bool canMoveCamera = true;
 
-	private Vector3 initialCameraPosition;
-    // Start is called before the first frame update
     void Start()
     {
-		initialCameraPosition = transform.position;
+        initialCameraPosition = transform.position;
     }
 
     private float cameraLeftEdge;
     private float cameraRightEdge;
-
     private float cameraWidth;
+
     void LateUpdate()
     {
-		UpdateCamera();
+        UpdateCamera();
     }
 
     // Update is called once per frame
@@ -39,25 +39,51 @@ public class CameraFollow : MonoBehaviour
     {
     }
 
-	void UpdateCamera()
-	{
+    private Vector3 lastPosition = new Vector3();
+
+    private int ComparePositionX(Vector3 pos1, Vector3 pos2)
+    {
+        if (pos1.x > pos2.x) return 1;
+        if (pos1.x < pos2.x) return -1;
+        return 0;
+    }
+
+    private enum Direcao { LTR, RTL }
+
+    void UpdateCamera()
+    {
         Vector3 middlePoint = (target1.position + target2.position) / 2f;
 
         // Calcula a posição desejada para a câmera
         desiredPosition = transform.position;
         desiredPosition.x = middlePoint.x;
-	
+
         // Utiliza o SmoothDamp para suavizar o movimento da câmera
         Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
-		
-	cameraRightEdge = Camera.main.ViewportToWorldPoint(new Vector3(1, 0.5f, smoothedPosition.x)).x;
-	cameraLeftEdge = Camera.main.ViewportToWorldPoint(new Vector3(0, 0.5f, smoothedPosition.x)).x;
-	
-	if ((cameraLeftEdge - wallLeft.position.x < 0.01f)) // ta parando aqui :(
-		return;
+
+        cameraRightEdge = Camera.main.ViewportToWorldPoint(new Vector3(1, 0.5f, smoothedPosition.x)).x;
+        cameraLeftEdge = Camera.main.ViewportToWorldPoint(new Vector3(0, 0.5f, smoothedPosition.x)).x;
+
+        Direcao direcao;
+
+        if (ComparePositionX(lastPosition, smoothedPosition) != 1)
+            direcao = Direcao.LTR;
+        else
+            direcao = Direcao.RTL;
+
+        Debug.Log(direcao);
+
+        if (direcao == Direcao.RTL)
+        {
+            if (wallLeft.position.x > cameraLeftEdge) return;
+        }
+        else if (direcao == Direcao.LTR)
+        {
+            if (wallRight.position.x < cameraRightEdge) return;
+        }
 
         // Atualiza a posição da câmera
         transform.position = smoothedPosition;
-	}
+        lastPosition = transform.position;
+    }
 }
-
