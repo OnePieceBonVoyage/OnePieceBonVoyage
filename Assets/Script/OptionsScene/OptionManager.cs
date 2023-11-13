6eb25cc87bad4c2e9888b6612282bec4e87037b4
smokeeaasd <lucas.gomes262@etec.sp.gov.br>
2023-11-13 15:26:57 -0300

    atualizando

--- Assets/Script/OptionsScene/OptionManager.cs
+++ Assets/Script/OptionsScene/OptionManager.cs
@@ -1,9 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class OptionManager : MonoBehaviour
 {
+    public Slider slider;
+    private float volText = 0;
+
+    public Text volumeLabel1;
+    public Text volumeLabel2;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +20,9 @@ public class OptionManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        volText = Mathf.Round(slider.value * 100);
+
+        volumeLabel1.text = $"Volume: {volText}";
+        volumeLabel2.text = $"Volume: {volText}";
     }
 }
